053807f55b062a4d7bca91612a856da3812cb465
Akuukis <akuukis@kalvis.lv>
2021-01-11 19:29:53 +0200

    chore: publish 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='beancount_plugin_utils',
-    version='0.0.3',
+    version='0.0.4',
     description='Utils for beancount plugin writers - BeancountError, mark, metaset, etc.',
 
     long_description=long_description,
